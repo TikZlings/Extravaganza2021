d726300e177fdfea4cf1e8e75dbaa600c7e68a93
samcarter <sam.carter8@aol.com>
2021-12-21 13:46:35 +0100

    changed starting point of music

--- samcarter/BrazilianFriend/convert2video.sh
+++ samcarter/BrazilianFriend/convert2video.sh
@@ -5,7 +5,7 @@ rm BrazilianFriend.mp4
 convert -density 240 BrazilianFriend.pdf BrazilianFriend.png
 
 # convert to video
-ffmpeg  -ss 00:00:00 -i BrazilianFriend-%d.png -ss 00:02:09 -i Simon_Garfunkel.m4a -shortest BrazilianFriend_raw.mp4
+ffmpeg  -ss 00:00:00 -i BrazilianFriend-%d.png -ss 00:02:25 -i Simon_Garfunkel.m4a -shortest BrazilianFriend_raw.mp4
 
 # repair video
 HandBrakeCLI --crop 0:0:0:0  -i BrazilianFriend_raw.mp4 -o BrazilianFriend.mp4
