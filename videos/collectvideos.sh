5896c969f6ffc7e0f2cd0ca1fb3e94b4d8a20234
samcarter <sam.carter8@aol.com>
2021-12-23 11:15:16 +0100

    title

--- videos/collectvideos.sh
+++ videos/collectvideos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 COUNTER=1
 
+cp ../intermissions/title.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../intermissions/NorthPole.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../samcarter/NorthPole/NorthPole.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../intermissions/almostsilentnight.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
@@ -15,47 +16,3 @@ cp ../intermissions/mice.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../samcarter/mouse/SingingMouse.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../intermissions/finale.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../carlatex/finale/granfinale.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-
-##cp ../intermissions/title_combined.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-##cp ../intermissions/title.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/bobbles.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../ulrike/bobble/bobble.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/connery.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../ulrike/seanconnery/seanconnery.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/bear.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../ulrike/honey/honey.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/baaa.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../samcarter/baaaaa/baaaaa.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/lochness.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../samcarter/lochness/lochness.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/venice.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../samcarter/venice/venice.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/hug.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../paulo/hug-the-world/hug-the-world.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/arts.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../samcarter/art/art.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/bees.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../samcarter/bees/bees.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/downunder.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../ulrike/downunder/downunder.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/zoom.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../paulo/zoom-meeting/tikzlings-meeting.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/007.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../plergux/007/007.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/xcoffins.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../paulo/xcoffins/xcoffins.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/queen.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../ulrike/queen/queen.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/vampire.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../samcarter/vampires/vampires.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/finale.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../ulrike/finale/finale.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#cp ../intermissions/credits.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-#
-#
-#for number in {1..33}
-#do
-#rm "$number".avi
-#ffmpeg -i "$number".mp4 "$number".avi
-#done
-
