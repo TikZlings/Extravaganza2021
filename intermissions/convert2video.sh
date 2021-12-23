20f0465cf30e953b1f701898049b6b57a1e512a3
samcarter <sam.carter8@aol.com>
2021-12-23 11:11:58 +0100

    title page

--- intermissions/convert2video.sh
+++ intermissions/convert2video.sh
@@ -72,13 +72,14 @@ repeatconvert(){
 }
 
 # Converting
-convert2video NorthPole "North Pole"
-convert2video almostsilentnight "Almost silent night"
-convert2video cello "Silent Night"
-convert2video BrazilianFriend "Brazilian Friend"
-convert2video wagner "Ride of the Valkyries"
-convert2video mice "Merry Christmas"
+#convert2video NorthPole "North Pole"
+#convert2video almostsilentnight "Almost silent night"
+#convert2video cello "Silent Night"
+#convert2video BrazilianFriend "Brazilian Friend"
+#convert2video wagner "Ride of the Valkyries"
+#convert2video mice "Merry Christmas"
+#convert2video finale "Grand Finale"
 
-#repeatconvert title "../ulrike/intermission/extravaganza-titlepage.pdf" 100
+repeatconvert title "../samcarter/title/extravaganza-titlepage.pdf" 100
 #repeatconvert credits "../ulrike/intermission/extravaganza-2020-credits.pdf" 175
 
--- videos/collectvideos.sh
+++ videos/collectvideos.sh
@@ -13,7 +13,8 @@ cp ../intermissions/wagner.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../ulrike/wagner/walkuere.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../intermissions/mice.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 cp ../samcarter/mouse/SingingMouse.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
-
+cp ../intermissions/finale.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
+cp ../carlatex/finale/granfinale.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 
 ##cp ../intermissions/title_combined.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
 ##cp ../intermissions/title.mp4 $COUNTER.mp4 ; COUNTER=$((COUNTER+1)) ;
